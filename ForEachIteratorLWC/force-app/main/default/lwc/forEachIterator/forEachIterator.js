10d5bdc60ae570ce6c4d1044ee555ae3ff78c950
Harshit Saxena <18752758+Harshit-Saxena@users.noreply.github.com>
2022-05-21 13:56:16 +0530

    Formated Code 'No functinal changes'

--- ForEachIteratorLWC/force-app/main/default/lwc/forEachIterator/forEachIterator.js
+++ ForEachIteratorLWC/force-app/main/default/lwc/forEachIterator/forEachIterator.js
@@ -1,34 +1,35 @@
-import { LightningElement, track } from 'lwc';
+import {
+    LightningElement,
+    track
+} from 'lwc';
 import getIteratorList from '@salesforce/apex/ForEachIteratorLightning.getIteratorList';
 export default class ForEachIterator extends LightningElement {
     @track accountList = [];
-    accountListObj = [
-        {
-            id: '01',
-            name: 'John Smith'
-        }, {
-            id: '02',
-            name: 'Rick Sean'
-        }, {
-            id: '03',
-            name: 'Jim Yang'
-        }, {
-            id: '04',
-            name: 'Richard livingston'
-        }, {
-            id: '06',
-            name: 'Ammy Gilbert'
-        }, {
-            id: '07',
-            name: 'Yograj Singh'
-        }, {
-            id: '08',
-            name: 'Victoria Palmer'
-        }, {
-            id: '09',
-            name: 'Joskhe Higashkhita'
-            }
-    ]
+    accountListObj = [{
+        id: '01',
+        name: 'John Smith'
+    }, {
+        id: '02',
+        name: 'Rick Sean'
+    }, {
+        id: '03',
+        name: 'Jim Yang'
+    }, {
+        id: '04',
+        name: 'Richard livingston'
+    }, {
+        id: '06',
+        name: 'Ammy Gilbert'
+    }, {
+        id: '07',
+        name: 'Yograj Singh'
+    }, {
+        id: '08',
+        name: 'Victoria Palmer'
+    }, {
+        id: '09',
+        name: 'Joskhe Higashkhita'
+    }]
     @track accountListServer = [];
     @track error = '';
     connectedCallback() {
@@ -45,7 +46,7 @@ export default class ForEachIterator extends LightningElement {
                 console.table(result);
                 console.log('result');
                 this.error = result;
-        })
+            })
     }
 
 }
